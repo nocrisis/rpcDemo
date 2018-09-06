ec015860dddc30b5a07a6f0d8bfb4ab7bc240164
15125296181@163.com <15125296181@163.com>
2018-09-06 18:04:47 +0800

    BIO

--- src/Consumer/rpcConsumer.java
+++ src/Consumer/rpcConsumer.java
@@ -8,9 +8,11 @@ public class rpcConsumer {
     public static void main(String[] args)throws Exception{
 //        FlyBehavior duckFlyBehavior = new DuckFlyBehaviorImpl();本地
         FlyBehavior duckFlyBehavior = rpcFrameworkSimple.refer(FlyBehavior.class,"192.168.2.63",666);
+        System.out.println("开始调用duck的fly方法");
         String duckResult = duckFlyBehavior.fly("家鸭");
 
         FlyBehavior swanFlyBehavior=rpcFrameworkSimple.refer(FlyBehavior.class,"192.168.2.63",555);
+        System.out.println("开始调用swan的fly方法");
         String swanResult = swanFlyBehavior.fly("天鹅");
 
         System.out.println(duckResult+","+swanResult);
