c8f58c2b65629f4175b973f85d1493242696f900
15125296181@163.com <15125296181@163.com>
2018-09-06 17:46:09 +0800

    second Impl

--- src/Consumer/rpcConsumer.java
+++ src/Consumer/rpcConsumer.java
@@ -6,8 +6,13 @@ import RPCFramwork.rpcFrameworkSimple;
 
 public class rpcConsumer {
     public static void main(String[] args)throws Exception{
+//        FlyBehavior duckFlyBehavior = new DuckFlyBehaviorImpl();本地
         FlyBehavior duckFlyBehavior = rpcFrameworkSimple.refer(FlyBehavior.class,"192.168.2.63",666);
-        String result = duckFlyBehavior.fly("家鸭");
-        System.out.println(result);
+        String duckResult = duckFlyBehavior.fly("家鸭");
+
+        FlyBehavior swanFlyBehavior=rpcFrameworkSimple.refer(FlyBehavior.class,"192.168.2.63",555);
+        String swanResult = swanFlyBehavior.fly("天鹅");
+
+        System.out.println(duckResult+","+swanResult);
     }
 }
--- src/Provider/rpcProvidor.java
+++ src/Provider/rpcProvidor.java
@@ -2,11 +2,14 @@ package Provider;
 
 import API.FlyBehavior;
 import API.impl.DuckFlyBehaviorImpl;
+import API.impl.SwanFlyBehaviorImpl;
 import RPCFramwork.rpcFrameworkSimple;
 
 public class rpcProvidor {
-    public static void main(String args[])throws Exception{
+    public static void main(String[] args)throws Exception{
         FlyBehavior duckFlyBehavior = new DuckFlyBehaviorImpl();
+        FlyBehavior swanFlyBehavior= new SwanFlyBehaviorImpl();
         rpcFrameworkSimple.export(duckFlyBehavior,666);
+        rpcFrameworkSimple.export(swanFlyBehavior,555);
     }
 }
