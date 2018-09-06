e372622d80ea84db0abbadfd655404269db491c2
15125296181@163.com <15125296181@163.com>
2018-09-06 15:22:33 +0800

    改名

--- src/Consumer/rpcConsumer.java
+++ src/Consumer/rpcConsumer.java
@@ -4,7 +4,7 @@ import API.FlyBehavior;
 import API.impl.DuckFlyBehaviorImpl;
 import RPCFramwork.RpcFrameworkSimple;
 
-public class RPCConsumer {
+public class rpcConsumer {
     public static void main(String[] args)throws Exception{
         FlyBehavior duckFlyBehavior= new DuckFlyBehaviorImpl();
         Object result = RpcFrameworkSimple.refer(FlyBehavior.class,"10.0.2.15",666);
--- src/Provider/rpcProvidor.java
+++ src/Provider/rpcProvidor.java
@@ -4,7 +4,7 @@ import API.FlyBehavior;
 import API.impl.DuckFlyBehaviorImpl;
 import RPCFramwork.RpcFrameworkSimple;
 
-public class RPCProvidor {
+public class rpcProvidor {
     public static void main(String args[])throws Exception{
         FlyBehavior duckFlyBehavior = new DuckFlyBehaviorImpl();
         RpcFrameworkSimple.export(duckFlyBehavior,666);
